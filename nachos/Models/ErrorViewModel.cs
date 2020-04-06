ae2119c981f14b84535b0fc9cb626c1170626cd2
gian@live.com.ph <gian@live.com.ph>
2020-04-06 04:47:57 -0400

    Initial check-in of module nachos

---
+++ nachos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace nachos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
